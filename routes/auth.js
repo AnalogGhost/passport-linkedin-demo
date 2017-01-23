e2586252ef42d4af562d0cd92e820e4d55bf5f80
Mathew Brown <matt@mattbrown.com>
2017-01-23 14:27:27 -0700

    Revert "Adding dotenv"
    This reverts commit 2ec2784d3804edff45c9ab3c3fa796027422c38a.

--- routes/auth.js
+++ routes/auth.js
@@ -7,10 +7,4 @@ router.get('/', function(req,res,next) {
     res.send('Auth');
 });
 
-router.get('/linkedin', 
-    passport.authenticate('linkedin'), 
-    function(req,res) {
-        //This will be redirected to LinkedIn, this function will not be called.
-    });
-
 module.exports = router;
