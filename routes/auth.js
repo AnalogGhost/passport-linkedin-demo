2ec2784d3804edff45c9ab3c3fa796027422c38a
Mathew Brown <matt@mattbrown.com>
2017-01-23 14:26:37 -0700

    Adding dotenv

--- routes/auth.js
+++ routes/auth.js
@@ -7,4 +7,10 @@ router.get('/', function(req,res,next) {
     res.send('Auth');
 });
 
+router.get('/linkedin', 
+    passport.authenticate('linkedin'), 
+    function(req,res) {
+        //This will be redirected to LinkedIn, this function will not be called.
+    });
+
 module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,21 @@
+require('dotenv').load();
 var express = require('express');
 var app = express();
 
+var passport = require('passport');
+var LinkedInStrategy = require('passport-linkedin').Strategy;
+
+app.use(passport.initialize());
+
+passport.use(new LinkedInStrategy({
+    },
+    function(token, tokenSecret, profile, done) {
+
+    })
+);
+
 var auth = require('./routes/auth.js');
+
 
 app.use('/auth', auth);
 
